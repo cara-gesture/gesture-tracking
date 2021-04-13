4a8a063b1af0a76c516c7cf338a82aba63477a22
Cara Dibdin <cdibdin@gmail.com>
2021-04-13 12:48:13 -0400

    updated routing

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
+
 ReactDOM.render(
-  <>
-    <App />
-  </>,
+  <BrowserRouter>
+    <Route exact path="/">
+      <Redirect to="/track" />
+    </Route>
+    <Route exact path="/track" render={() => <App />} />
+  </BrowserRouter>,
   document.getElementById("root")
 );
